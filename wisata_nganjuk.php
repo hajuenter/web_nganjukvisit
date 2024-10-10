d4e4939c5330c08919d9cd6d305d742711785595
TheHajuenter <esjeruk517@gmail.com>
2024-10-10 21:58:42 +0700

    fix detail wisata tampilan lp

--- wisata_nganjuk.php
+++ wisata_nganjuk.php
@@ -1,6 +1,5 @@
 <?php
 include("koneksi.php");
-
 ?>
 
 <!DOCTYPE html>
@@ -57,29 +56,59 @@ include("koneksi.php");
     </nav>
     <!-- navbar end -->
 
-    <div class="container pt-5 mt-5 pt-lg-5 mt-lg-5">
-        <div class="card rounded mb-3 p-md-3 mt-5 pt-5 cardfillcolor">
-            <div class="row g-0 align-items-center">
-                <div class="col-md-4 d-flex align-items-center justify-content-center image-container-cover">
-                    <img src="" class="gambar-cover img-fluid align-self-center" alt="cover-image">
+    <div class="container pt-5 mt-5 pt-lg-5 mt-lg-5 mb-3">
+        <div class="row">
+            <!-- Image Slider -->
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <div id="wisataSlider" class="carousel slide mt-4" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#wisataSlider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#wisataSlider" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#wisataSlider" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <!-- Slide 1 -->
+                        <div class="carousel-item active">
+                            <img src="./public/gambar/lupa_password.drawio.png" class="d-block mx-auto" style="max-width: 80%;" alt="Gambar Wisata 1">
+                        </div>
+                        <!-- Slide 2 -->
+                        <div class="carousel-item">
+                            <img src="./public/gambar/login.drawio.png" class="d-block mx-auto" style="max-width: 80%;" alt="Gambar Wisata 2">
+                        </div>
+                        <!-- Slide 3 -->
+                        <div class="carousel-item">
+                            <img src="./public/gambar/dashboard.drawio.png" class="d-block mx-auto" style="max-width: 80%;" alt="Gambar Wisata 3">
+                        </div>
+                    </div>
+                    <!-- Previous Button -->
+                    <button class="carousel-control-prev" type="button" data-bs-target="#wisataSlider" data-bs-slide="prev" style="color: black; font-size: 2rem;">
+                        <span class="carousel-control-prev-icon visually-hidden">Prev</span>
+                        <span aria-hidden="true">&lt;</span> <!-- Icon "<" -->
+                    </button>
+                    <!-- Next Button -->
+                    <button class="carousel-control-next" type="button" data-bs-target="#wisataSlider" data-bs-slide="next" style="color: black; font-size: 2rem;">
+                        <span class="carousel-control-next-icon visually-hidden">Next</span>
+                        <span aria-hidden="true">&gt;</span> <!-- Icon ">" -->
+                    </button>
                 </div>
-                <div class="col-md-8">
+            </div>
+
+            <!-- Descriptions -->
+            <div class="col-lg-6 col-md-6 col-sm-12 mt-4">
+                <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Nama:</h5>
-                        <p class="card-text"></p>
-                        <h5 class="card-title">Alamat:</h5>
-                        <p class="card-text"></p>
-                        <h5 class="card-title">Jam Operasional:</h5>
-                        <p class="card-text"></p>
-                        <h5 class="card-title">Harga Tiket:</h5>
-                        <p class="card-text"></p>
-                        <h5 class="card-title">Deskripsi:</h5>
-                        <p class="card-text"></p>
+                        <h5 class="card-title">Nama Wisata: Wisata Alam</h5>
+                        <p class="card-text"><strong>Alamat:</strong> Jalan Pegunungan, Kota A</p>
+                        <p class="card-text"><strong>Jam Operasional:</strong> 08:00 - 17:00</p>
+                        <p class="card-text"><strong>Harga Tiket:</strong> Rp 50.000</p>
+                        <p class="card-text"><strong>Deskripsi:</strong> Wisata alam ini menawarkan pemandangan yang luar biasa dan udara yang sejuk. Sangat cocok untuk tempat rekreasi bersama keluarga dan teman-teman.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <hr>
+
 
     <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
