7185d1bc79da75f8a4a785bc2dd226a5f6efad87
TheHajuenter <esjeruk517@gmail.com>
2024-10-19 22:38:07 +0700

    fix bug nav index

--- index.php
+++ index.php
@@ -30,43 +30,43 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
 </head>
 
 <body>
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg bg-white shadow-sm z-3 fixed-top" style="z-index: 1050;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="./img/logo_nav.png" alt="logo" class="ms-lg-5 img-fluid">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 100px;">
-                    <li class="nav-item">
-                        <a class="nav-link ms-lg-5" aria-current="page" href="#home">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ms-lg-3" aria-current="page" href="#kategori">Kategori</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle ms-lg-3" role="button" data-bs-toggle="dropdown" aria-expanded="false">Detail</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="wisata_nganjuk.php">Wisata</a></li>
-                            <li><a class="dropdown-item" href="kuliner_nganjuk.php">Kuliner</a></li>
-                            <li><a class="dropdown-item" href="hotel_nganjuk.php">Penginapan</a></li>
-                            <li><a class="dropdown-item" href="event_nganjuk.php">Event</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link ms-lg-3" aria-current="page" href="#tentang_kami">Tentang Kami</a>
-                    </li>
-                </ul>
-                <div class="d-flex">
-                    <a href="login.php" class="btn me-lg-5 btn-outline-primary mt-2 mt-lg-0 mx-auto px-5 px-lg-3">Login</a>
-                </div>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg bg-white shadow-sm z-3 fixed-top" style="z-index: 1050;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="./img/logo_nav.png" alt="logo" class="ms-lg-5 img-fluid">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link ms-lg-5" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ms-lg-3" aria-current="page" href="#kategori">Kategori</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle ms-lg-3" role="button" data-bs-toggle="dropdown" aria-expanded="false">Detail</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="wisata_nganjuk.php">Wisata</a></li>
+                        <li><a class="dropdown-item" href="kuliner_nganjuk.php">Kuliner</a></li>
+                        <li><a class="dropdown-item" href="hotel_nganjuk.php">Penginapan</a></li>
+                        <li><a class="dropdown-item" href="event_nganjuk.php">Event</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link ms-lg-3" aria-current="page" href="#tentang_kami">Tentang Kami</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                <a href="login.php" class="btn me-lg-5 btn-outline-primary mt-2 mt-lg-0 mx-auto px-5 px-lg-3">Login</a>
             </div>
         </div>
-    </nav>
-    <!-- navbar end -->
+    </div>
+</nav>
+<!-- navbar end -->
 
     <!-- header -->
     <header id="home" class="pt-5 z-1 container-fluid pb-5">
--- kuliner_nganjuk.php
+++ kuliner_nganjuk.php
@@ -40,7 +40,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 100px;">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link ms-lg-5" aria-current="page" href="index.php#home">Home</a>
                     </li>
