5b1474fde48700514ac433aa1d8ceca3343bff5f
TheHajuenter <esjeruk517@gmail.com>
2024-10-20 15:15:04 +0700

    fix set wisata untuk tiket

--- controllers/set_wisata.php
+++ controllers/set_wisata.php
@@ -9,9 +9,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id_wisata = $_POST['wisata'];
 
     // Update ket_wisata di tabel user
-    $query = "UPDATE user SET ket_wisata = '$id_wisata' WHERE id_user = '$id_pengelola'";
-    if (mysqli_query($conn, $query)) {
-        $_SESSION['success_konfir'] = 'Wisata berhasil diset untuk pengelola.';
+    $query_user = "UPDATE user SET ket_wisata = '$id_wisata' WHERE id_user = '$id_pengelola'";
+    if (mysqli_query($conn, $query_user)) {
+        // Update id_pengelola di tabel detail_wisata
+        $query_wisata = "UPDATE detail_wisata SET id_pengelola = '$id_pengelola' WHERE id_wisata = '$id_wisata'";
+        if (mysqli_query($conn, $query_wisata)) {
+            $_SESSION['success_konfir'] = 'Wisata berhasil diset untuk pengelola.';
+        } else {
+            $_SESSION['error_konfir'] = 'Gagal mengupdate pengelola pada tabel detail_wisata.';
+        }
     } else {
         $_SESSION['error_konfir'] = 'Gagal mengupdate wisata.';
     }
